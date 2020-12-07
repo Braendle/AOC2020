b64832da12ec79014f8486840d0151ce24d5bba9
Marian Brändle <marianbraendle@gmx.de>
2020-12-07 17:29:24 +0100

    CHG: added output type hint

--- solutions/common.py
+++ solutions/common.py
@@ -1,12 +1,12 @@
+from typing import Any, Dict, List
+
 import parse
 
 
-def parselines(parse_format: str, data: str):
+def parselines(parse_format: str, data: str) -> List[Dict[str, Any]]:
     lines = data.splitlines()
     format_compiled = parse.compile(parse_format)
     try:
-        parse_res = [format_compiled.parse(line).named for line in lines]
+        return [format_compiled.parse(line).named for line in lines]
     except AttributeError:
         raise SyntaxError("Parse format '%s' does not match file content of file '%s'" % (parse_format, data))
-
-    return parse_res
