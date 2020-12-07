1afeaa6c49358389c292d9cab14f9357d5e746e2
Marian Brändle <marianbraendle@gmx.de>
2020-12-07 17:28:34 +0100

    CHG: cleanup

--- solutions/07.py
+++ solutions/07.py
@@ -12,19 +12,19 @@ import networkx as nx
 
 # ======= Helper Functions/Classes ======= #
 def get_num_contains(G, cur_node):
-    return 1 + sum(G[cur_node][s]["n_children"]*get_num_contains(G, s) for s in G.successors(cur_node))
+    return 1 + sum(G[cur_node][s]["count"]*get_num_contains(G, s) for s in G.successors(cur_node))
 
 
 # ============= Input Data ============= #
 raw_data = Path("./inputs/07/input.txt").read_text()
-# Let's try to solve the puzzle by using networkx. Might be easier to do it by hand...
+# Let's learn a new library and try to solve the puzzle by using networkx. In the end it might be easier to do it by hand...
 edges = []
 for line in raw_data.splitlines():
     p, cs = line.split(" bags contain ")
     if not cs.startswith("no other"):
         for c in cs.split(", "):
             grps = re.match(r"^(\d+) (.+?) bag.{,2}$", c).groups()
-            edges.append((p, grps[1], {'n_children': int(grps[0])}))
+            edges.append((p, grps[1], {'count': int(grps[0])}))
 G = nx.DiGraph(edges)
 
 
